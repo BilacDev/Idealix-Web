56e072478dbc43305758b438d1f9990275972d41
riwersxd <guilherme.rc.98@gmail.com>
2019-11-10 01:59:19 -0300

    Ajusta menu do usuario

--- src/components/ProfileHeader.vue
+++ src/components/ProfileHeader.vue
@@ -2,19 +2,21 @@
   <header class="profileheader-component">
     <md-menu md-align-trigger md-direction="bottom-end" md-size="medium">
       <div class="profileheader-component__menu-button md-button"  md-menu-trigger>
-        <user-avatar :name="name" :picture="picture" class="profileheader-component__avatar-icon" />
-        <span class="profileheader-component__user-name">{{ name }}</span>
+        <user-avatar :name="mainNames" :picture="userInfo.picture" class="profileheader-component__avatar-icon" />
+        <span class="profileheader-component__user-name">{{ mainNames }}</span>
         <md-icon>arrow_drop_down</md-icon>
       </div>
       <md-menu-content>
         <md-menu-item class="profileheader-component__action-icon" @click="">
           <md-icon>person</md-icon>
-          Perfil
+          <span>Perfil</span>
         </md-menu-item>
         <md-divider/>
         <md-menu-item class="profileheader-component__action-icon" @click="">
           <md-icon>directions_run</md-icon>
-          Sair
+          <span>
+            Sair
+          </span>
         </md-menu-item>
       </md-menu-content>
     </md-menu>
@@ -26,18 +28,19 @@ import UserAvatar from '@/components/UserAvatar'
 
 export default {
   name: 'ProfileHeader',
-  components: {
-    UserAvatar
-  },
+  components: { UserAvatar },
   data: () => ({
-    tmpname: 'Guilherme Rios da Cunha',
-    // picture: 'https://placeimg.com/40/40/people/10'
+    userInfo: {
+      id: 1,
+      name: 'Guilherme Rios da Cunha',
+      picture: 'https://placeimg.com/40/40/people/10'
+    }
   }),
   computed: {
-    name () {
-      let names = this.tmpname.split(' ')
-      let name = names[0] + ' ' + names[names.length - 1]
-      return name
+    mainNames () {
+      let fullName = this.userInfo.name.split(' ')
+      let mainNames = fullName[0] + ' ' + fullName[fullName.length - 1]
+      return mainNames
     }
   }
 }
@@ -54,6 +57,7 @@ export default {
   height:40px;
   margin: 20px 16px;
   &__menu-button {
+    cursor: pointer;
     width: auto;
     height: auto;
     display: flex;
@@ -65,20 +69,17 @@ export default {
   }
   &__user-name, .md-icon {
     font-size: 1.4em;
-    color: $--text-default !important;
+    color: $--default-text !important;
   }
   &__action-icon {
-    text-align: left;
     .md-list-item-content {
       justify-content: flex-start;
+      font-family: 'Montserrat', sans-serif;
     }
     .md-icon {
-      margin: 0px 10px 0px 0px !important;
-      // color: $--text-default !important;
+      margin-right: 8px !important;
+      color: $--accent-text !important;
     }
-  }
-  &__avatar-icon {
-    background-color: #e0bce6 !important;
   }
 }
 </style>
