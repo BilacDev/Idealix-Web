67338405871f3340ddcec822690d745dab8a1b2b
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-11-11 10:38:56 -0200

    Cria card de graficos

--- src/views/partials/Dashboard.vue
+++ src/views/partials/Dashboard.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="dashboard-view">
-    <md-card class="dashboard-view__details-card elevation">
-      <user-avatar :name="name" :picture="picture" size="md-large" />
-      <div class="dashboard-view__details-content">
-        <span>{{ name }}</span>
-        <span>{{ age }} Anos - {{ gender }}</span>
-      </div>
-    </md-card>
-    <md-card class="dashboard-view__details-card elevation">
-      <user-avatar icon="favorite" size="md-large" />
-      <div class="dashboard-view__details-content">
-        <span>Status Atual</span>
-        <span>Saldavel</span>
-      </div>
-    </md-card>
-    <md-card class="dashboard-view__details-card elevation">
-      <user-avatar icon="favorite" size="md-large" />
-      <div class="dashboard-view__details-content">
-        <span>Ultimo marco</span>
-        <span>Peso: 22,16Kg - Altura: 1,25m</span>
-      </div>
-    </md-card>
+    <div class="dashboard-view__top-cards">
+      <md-card class="dashboard-view__details-card elevation">
+        <user-avatar :name="name" :picture="picture" size="md-large" />
+        <div class="dashboard-view__details-content">
+          <span>{{ name }}</span>
+          <span>{{ age }} Anos - {{ gender }}</span>
+        </div>
+      </md-card>
+      <md-card class="dashboard-view__details-card elevation">
+        <user-avatar icon="favorite" size="md-large" />
+        <div class="dashboard-view__details-content">
+          <span>Status Atual</span>
+          <span>Saldavel</span>
+        </div>
+      </md-card>
+      <md-card class="dashboard-view__details-card elevation">
+        <user-avatar icon="favorite" size="md-large" />
+        <div class="dashboard-view__details-content">
+          <span>Ultimo marco</span>
+          <span>Peso: 22,16Kg - Altura: 1,25m</span>
+        </div>
+      </md-card>
+    </div>
     <md-card class="dashboard-view__chart-card elevation">
 
     </md-card>
@@ -47,15 +49,16 @@ export default {
 <style lang="scss">
 @import '../../styles/colors.scss';
 
-.card {
-  // box-shadow: 0 0 15px 0 $--black;
-  box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
-}
 .dashboard-view {
   padding: 0px 16px 16px 16px;
   display: flex;
+  flex-direction: column;
+  flex: 1;
+  &__top-cards {
+    display: flex;
+    margin-bottom: 16px;
+  }
   &__details-card {
-    // width: 33.3%;
     flex: 1;
     display: flex;
     align-items: center;
@@ -78,9 +81,11 @@ export default {
     }
   }
   &__chart-card {
+    align-items: stretch;
+    align-self: stretch;
     display: flex;
     flex: 1;
-    width: 100%
+    border-radius: $--border-radius;
   }
 }
 </style>
